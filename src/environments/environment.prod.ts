9a32c73c6f7b7d391a5e57d2ae27621e0d9eb427
Abner F <abnerfs@gmail.com>
2020-08-29 14:50:36 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   SERVER_LINK: 'https://abnerfs-spotify-api.herokuapp.com',
-  REDIRECT_URI: 'https://abnerfs-spotify-ui.herokuapp.com/callback'
+  REDIRECT_URI: 'https://spotify.abnerfs.dev/callback'
 };
